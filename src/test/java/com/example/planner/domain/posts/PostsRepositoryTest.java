edf281e9646556b60861ae786e77bf7e3ecc076a
kapr <kyowon1108@gmail.com>
2024-01-15 12:19:08 +0900

    test

--- src/test/java/com/example/planner/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/example/planner/domain/posts/PostsRepositoryTest.java
@@ -15,7 +15,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class PostsRepositoryTest {
-
     @Autowired
     PostsRepository postsRepository;
 
