c6109ebad42f47660869653bccce40e0f97b0af4
kapr <kyowon1108@gmail.com>
2024-01-17 11:59:36 +0000

    test

---
+++ src/main/java/com/example/planner/web/PostsApiController.java
@@ -0,0 +1,46 @@
+package com.example.planner.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.planner.service.PostsService;
+import com.example.planner.web.dto.PostsResponseDto;
+import com.example.planner.web.dto.PostsSaveRequestDto;
+import com.example.planner.web.dto.PostsUpdateRequestDto;
+import com.example.planner.web.dto.PostsListResponseDto;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
